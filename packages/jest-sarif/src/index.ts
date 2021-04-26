c6ba2d08dea0b4ef553b1ca46f90e4b7962abbff
Steve Calvert <scalvert@linkedin.com>
2021-04-26 16:11:05 -0700

    Removing unused type

--- packages/jest-sarif/src/index.ts
+++ packages/jest-sarif/src/index.ts
@@ -1,1 +1,3 @@
 export * from './matchers';
+
+export type { Definition } from './types';
--- packages/jest-sarif/src/types/index.ts
+++ packages/jest-sarif/src/types/index.ts
@@ -1,11 +1,1 @@
-import { Definition } from './definition';
-
 export * from './definition';
-
-/**
- * Matcher options provided to the buildMatcher function in order to build a dynamic Jest matcher.
- */
-export interface BuildMatcherOptions {
-  matcherName: string;
-  definition?: Definition;
-}
