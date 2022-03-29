fac247e62264e06881cb83e5a94f79ffce2f133b
Yong Yan <yanyong-gh@outlook.com>
2022-03-29 13:46:26 -0700

    Merge latest changes

--- packages/eslint-formatter-sarif/sarif.js
+++ packages/eslint-formatter-sarif/sarif.js
@@ -6,11 +6,11 @@
 
 'use strict';
 
-const lodash = require('lodash');
 const fs = require('fs');
-const utf8 = require('utf8');
-const jschardet = require('jschardet');
 const url = require('url');
+const utf8 = require('utf8');
+const lodash = require('lodash');
+const jschardet = require('jschardet');
 
 //------------------------------------------------------------------------------
 // Helper Functions
@@ -95,9 +95,9 @@ module.exports = function (results, data) {
   let executionSuccessful = true;
 
   for (const result of results) {
-    // Only add it if not already there.
-    if (typeof sarifFiles[result.filePath] === 'undefined') {
-      sarifArtifactIndices[result.filePath] = nextArtifactIndex++;
+        // Only add it if not already there.
+        if (typeof sarifFiles[result.filePath] === 'undefined') {
+            sarifArtifactIndices[result.filePath] = nextArtifactIndex++;
 
       let contentsUtf8;
 
@@ -128,8 +128,12 @@ module.exports = function (results, data) {
           console.log(error);
         }
       }
-      if (result.messages.length > 0) {
-        for (const message of result.messages) {
+
+      const containsSuppressedMessages = result.suppressedMessages && result.suppressedMessages.length > 0;
+      const messages = containsSuppressedMessages ? result.messages.concat(result.suppressedMessages) : result.messages;
+
+      if (messages.length > 0) {
+        for (const message of messages) {
           const sarifRepresentation = {
             level: getResultLevel(message),
             message: {
@@ -187,6 +191,17 @@ module.exports = function (results, data) {
             if (sarifRuleIndices[message.ruleId] !== 'undefined') {
               sarifRepresentation.ruleIndex = sarifRuleIndices[message.ruleId];
             }
+
+            if (containsSuppressedMessages) {
+              sarifRepresentation.suppressions = message.suppressions ?
+                message.suppressions.map(suppression => {
+                  return {
+                    kind: suppression.kind === 'directive' ? 'inSource' : 'external',
+                    justification: suppression.justification,
+                  }
+                }) :
+                [];
+            }
           } else {
             // ESLint produces a message with no ruleId when it encounters an internal
             // error. SARIF represents this as a tool execution notification rather
@@ -213,6 +228,12 @@ module.exports = function (results, data) {
             }
             if (message.column > 0) {
               sarifRepresentation.locations[0].physicalLocation.region.startColumn = message.column;
+            }
+            if (message.endLine > 0) {
+              sarifRepresentation.locations[0].physicalLocation.region.endLine = message.endLine;
+            }
+            if (message.endColumn > 0) {
+              sarifRepresentation.locations[0].physicalLocation.region.endColumn = message.endColumn;
             }
           }
 
